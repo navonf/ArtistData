a42c255250c9a53e4c6bc2ba98013447388d7693
Navon Francis <navonfrancis@gmail.com>
2018-07-11 00:33:37 -0400

    okokok

--- app.py
+++ app.py
@@ -6,16 +6,12 @@ import scraper
 app = Flask(__name__)
 api = Api(app)
 
-
 class ArtistData(Resource):
 
     def get(self, name):
         return scraper.base_call(name), 200
 
-
-
 api.add_resource(ArtistData, "/artistdata/<string:name>")
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
