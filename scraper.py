a01ba55221ece3207e05a2cd50041fa3a13bda41
Navon Francis <navonfrancis@gmail.com>
2018-07-11 23:57:12 -0400

    added multithreading for each page use.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
-
+from queue import Queue
 import grequests
 import requests
+import threading
 import json
 import lxml
 import re
@@ -13,7 +14,7 @@ songs_names_list = []
 songs_ratings_list = []
 base_url = 'https://www.hotnewhiphop.com'
 artist_name = ''
-
+artist_data_lock = threading.Lock()
 
 def base_call(artistname):
     # Test data
@@ -35,14 +36,14 @@ def base_call(artistname):
     print(artist_name)
 
     # get the page count so we can iterate through pages.
-    artist_all_songs_page_count = get_all_page_count(songs_list_base_soup, artist_data)
-    print(json.dumps(artist_data, indent=4, sort_keys=True))
+    artist_all_songs_page_count = get_all_page_count(songs_list_base_soup, artist_data, songs_list_base_url)
+    print(artist_data)
     return artist_data
-
 
 
 # iterate through the artists with multiple pages.
 def iterate_pages(soup, page_count, artist_data):
+
     for i in range(0, int(page_count)+1):
         get_page_data(soup, artist_data)
 
@@ -50,6 +51,60 @@ def iterate_pages(soup, page_count, artist_data):
             next_page_url = base_url + soup.find('a', class_="next-page", href=True)['href']
             next_page_req = requests.get(next_page_url)
             soup = BeautifulSoup(next_page_req.text, 'lxml')
+
+
+def batch_job(worker, soup, artist_data, next_page_urls_list):
+    with artist_data_lock:
+        curr = next_page_urls_list.get()
+        print(curr)
+        req = requests.get(curr)
+        soup_thread = BeautifulSoup(req.text, 'lxml')
+
+        relevant_songs = list(filter(lambda x: True if artist_name in str(x.find('div', class_="endlessScrollCommon-artist")) else False,
+        soup_thread.find_all('div', class_="endlessScrollCommon-title song")))
+
+        songs_on_page = []
+
+        for song in relevant_songs:
+            songs_on_page.append(base_url + str(song.find('a', class_="endlessScrollCommon-title-anchor")['href']))
+
+        artist_data.append(songs_on_page)
+
+
+
+
+def threader(q, soup, artist_data, next_page_urls_list):
+    while True:
+        worker = q.get()
+        batch_job(worker, soup, artist_data, next_page_urls_list)
+        q.task_done()
+
+
+def iterate_pages_batch(soup, page_count, artist_data, songs_list_base_url):
+
+    next_page_urls_list = Queue()
+    next_page_urls_list.put(songs_list_base_url)
+
+
+    for i in range(1, int(page_count)+1):
+        next_page_urls_list.put(songs_list_base_url + str(i) + '/')
+
+    # TODO: spawn a thread for each link in the list
+    #  shared varilable that will be updated by all the threads
+    # artist_data_lock = threading.Lock()
+    q = Queue()
+
+    for x in range(next_page_urls_list.qsize()):
+        t = threading.Thread(target = threader, kwargs={'q' : q, 'soup' : soup, 'artist_data' : artist_data, 'next_page_urls_list' : next_page_urls_list})
+        t.daemon = True
+        t.start()
+
+    for worker in range(next_page_urls_list.qsize()):
+        q.put(worker)
+
+    q.join()
+
+
 
 def get_single_page_data(soup, artist_data):
 	songs_on_page = list(map(lambda u: base_url + u['href'],
@@ -92,17 +147,21 @@ def get_song_data(soup, artist_data):
     artist_data.append(data)
 
 
-
 # gets all the pages the artist has.
-def get_all_page_count(soup, artist_data):
+def get_all_page_count(soup, artist_data, songs_list_base_url):
     # find the link in which the page count is contained.
     if soup.find('a', class_="last-page", href=True) is None:
         get_single_page_data(soup, artist_data)
     else:
         page_link = soup.find('a', class_="last-page", href=True)['href']
+
         # extract the digit in the string.
-        print("multiple pages")
-        iterate_pages(soup, re.findall('\d+', page_link)[0], artist_data)
+        page_count = int(re.findall('\d+', page_link)[0])
+
+        # do batch requests if the artist has more than 5 pages
+        iterate_pages_batch(soup, page_count, artist_data, songs_list_base_url)
+        # else: iterate_pages(soup, page_count, artist_data)
+
 
 # if __name__ == '__main__':
-#     base_call('frankocean')
+#     base_call('migos')
